f6a3a2d98c6cbe93727961da1ca0deaf83f9eaed
David-Shibley <davidjshibley@gmail.com>
2016-01-29 10:43:12 -0700

    setup file structure and routes. added http request to server

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,14 @@
-var app = angular.module('myApp', []);
+var app = angular.module('bankRobberyApp', ["ngRoute", "ui.bootstrap"]);
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'partials/home.html',
+    controller: 'homeController'
+  }).when('/game', {
+    templateUrl: 'partials/game.html',
+    controller: 'gameController'
+  }).when('/win', {
+    templateUrl: 'partials/win.html',
+    controller: 'winController'
+  }).otherwise('/');
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,16 @@
-app.controller('myController', function($scope) {
-  $scope.greeting = "Hello World!";
+app.controller('mainController', function($scope) {
+});
+
+app.controller('winController', function($scope, $http){
+  $http.get('https://secret12.herokuapp.com/secretinfo').then(function(data){
+    $scope.winData = data.data;
+  });
+});
+
+app.controller('gameController', function($scope, $http){
+
+});
+
+app.controller('homeController', function($scope, $http){
+
 });
