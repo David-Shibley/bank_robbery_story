7337260c0fbe653c0da146d13e42757f724086a8
David-Shibley <davidjshibley@gmail.com>
2016-01-29 11:47:54 -0700

    added some functionality to the win page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,6 @@ app.controller('winController', function($scope, $http){
   $http.get('https://secret12.herokuapp.com/secretinfo').then(function(data){
     $scope.winData = data.data;
     $scope.winnings = data.data.secretData;
-    $scope.waffles = data.data.secretString;
   });
 });
 
